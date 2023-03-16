b9a2d00da30e8e97501eec73e5be249303c8a4c8
Harold Adrian <haroldb@mill5.com>
2023-03-15 19:19:14 -0500

    Add Basic Scenary to throw a thread on java

---
+++ 07_pgm_thread/src/co/edu/campusucc/poo/MainThreadExtends.java
@@ -0,0 +1,12 @@
+package co.edu.campusucc.poo;
+
+import co.edu.campusucc.poo.domain.ThreadExtends;
+
+public class MainThreadExtends{
+    public static void main(String[] args)
+    {
+        ThreadExtends thread = new ThreadExtends();
+        // Invoking Thread using start() method
+        thread.start();
+    }
+}
---
+++ 07_pgm_thread/src/co/edu/campusucc/poo/domain/ThreadExtends.java
@@ -0,0 +1,8 @@
+package co.edu.campusucc.poo.domain;
+
+public class ThreadExtends extends Thread{
+    public void run()
+    {
+        System.out.println("Thread Started Running...on UCC University");
+    }
+}
