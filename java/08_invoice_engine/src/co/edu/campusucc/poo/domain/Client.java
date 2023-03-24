05eaaad8aea06a6dcd98aba2d3d5c7935a841fae
Harold Adrian <haroldb@mill5.com>
2023-03-24 15:21:51 -0500

    Set fields to Client

--- java/08_invoice_engine/src/co/edu/campusucc/poo/domain/Client.java
+++ java/08_invoice_engine/src/co/edu/campusucc/poo/domain/Client.java
@@ -1,5 +1,79 @@
 package co.edu.campusucc.poo.domain;
 
 public class Client {
+    private String id;
+    private String typeIdCard;
+    private String idCard;
+    private String name;
+    private String nickName;
+    private String lastname;
+
+    public Client(String id, String typeIdCard, String idCard, String name, String nickName, String lastname) {
+        this.id = id;
+        this.typeIdCard = typeIdCard;
+        this.idCard = idCard;
+        this.name = name;
+        this.nickName = nickName;
+        this.lastname = lastname;
+    }
+
+    public Client() {
+    }
+
+    @Override
+    public String toString() {
+        return "Client [id=" + id + ", typeIdCard=" + typeIdCard + ", idCard=" + idCard + ", name=" + name
+                + ", nickName=" + nickName + ", lastname=" + lastname + "]";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTypeIdCard() {
+        return typeIdCard;
+    }
+
+    public void setTypeIdCard(String typeIdCard) {
+        this.typeIdCard = typeIdCard;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
     
 }
+
